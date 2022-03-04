736fef0f566c6eb70cdd2455590ba4ad249d3fe8
m.buscemi <MBuscemi1996>
2022-03-04 16:15:13 +0100

    initial commit

--- Labirinto/GameScene.swift
+++ Labirinto/GameScene.swift
@@ -1,109 +1,59 @@
 //
 //  GameScene.swift
-//  Labirinto
-//
-//  Created by Mauro Buscemi on 04/03/22.
+//  provagioco1
+//a
+//  Created by Aurora Messina queen regin on 04/02/22.
 //
 
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
 
-        self.lastUpdateTime = 0
+//definisci i nodi e le loro proprietà
+
+
+class GameScene: SKScene {
+    let nodo = SKNode ()
+    let sfondo = SKSpriteNode(imageNamed: "sfondi")
+    let scritta = SKLabelNode(text: "ok")
+    
+    
+    
+    let tasti =  SKSpriteNode(imageNamed: "pulsanti");
+    
+    //appendi i nodi, gestisci posizione e dimensione
+    //con questa funzione carichiamo la schermata
+    override func sceneDidLoad() {
+        addChild(sfondo)
+        addChild(tasti)
+        addChild(scritta)
+        sfondo.position = CGPoint(x: frame.midX, y: frame.midY)
+        tasti.position = CGPoint(x: frame.midX, y: frame.midY)
+        sfondo.zPosition = -1
+        tasti.zPosition = 1
+        scritta.zPosition = 2
+        scritta.fontSize = 25.0
+        scritta.fontColor = .blue
+        scritta.fontName = "Futura Bold"
+        scritta.position = CGPoint(x: tasti.frame.midX, y: tasti.frame.midY )
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+    //con le prime due variabili intercettiamo informazione sul punto
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        scritta.run(SKAction.move(to: CGPoint(x: frame.midX, y: (tasti.position.y + 40.0)), duration: 2.0))
+        
+        let tocco = touches.first
+        let posizione = tocco?.location (in: self)
+        
+        let nodoToccato = nodes(at: posizione!)
+        
+        if (tasti.position.x == nodoToccato.first?.position.x && tasti.position.y == nodoToccato.first?.position.y)
+        { scene?.view?.presentScene(MappaScene())
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
         
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
     }
 }
---
+++ Labirinto/Scena 1.swift
@@ -0,0 +1,16 @@
+//
+//  Scena 1.swift
+//  labririnto definitivo
+//
+//  Created by labMiT02 on 04/03/22.
+//
+
+import Foundation
+import SpriteKit
+
+class MappaScene: SKScene {
+    
+    override func sceneDidLoad() {
+    }
+    
+}
